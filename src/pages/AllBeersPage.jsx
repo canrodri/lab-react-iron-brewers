253db8b7cd3e03dd0fa9002a2a2f0c8783f0dcf4
candela <candelap.r.p@gmail.com>
2025-01-29 19:32:50 +0100

    [WDPTRMT]-candela rodriguez

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "../components/Search";
 import beersJSON from "./../assets/beers.json";
+import axios from 'axios';
+import { useActionState } from "react";
 
 
 
@@ -15,6 +17,18 @@ function AllBeersPage() {
   // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
+useEffect(() => {
+  const fetchBeers = () => {
+    axios.get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching beers', error)
+      })
+  }
+  fetchBeers();
+}, []);
 
 
 
--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
+import axios from "axios";
 
 
 function BeerDetailsPage() {
@@ -14,10 +15,26 @@ function BeerDetailsPage() {
 
   // TASKS:
   // 1. Get the beer ID from the URL, using the useParams hook.
+  const { beerId } = useParams;
+
   // 2. Set up an effect hook to make a request for the beer info from the Beers API.
   // 3. Use axios to make a HTTP request.
   // 4. Use the response data from the Beers API to update the state variable.
 
+  useEffect(() => {
+    const getBeerDetails = () => {
+      axios
+        .get(`https://ih-beers-api2.herokuapp.com/beers/${beerId}`) // Hacer la petición HTTP
+        .then((response) => {
+          setBeer(response.data); // Actualizar el estado con los datos de la API
+        })
+        .catch((error) => {
+          console.error('Error fetching beer details:', error);
+        });
+    };
+
+    getBeerDetails(); // Llamar a la función
+  }, [beerId]); // Dependencia: se ejecuta cuando beerId cambia
 
 
   // Structure and the content of the page showing the beer details. You can leave this as it is:
